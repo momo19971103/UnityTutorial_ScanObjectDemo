dd28e2fcdcad2200af682266f5e078f9359ca6ce
momo19971103 <momo19971103@yahoo.com.tw>
2019-11-02 00:09:25 +0800

    跳躍動作

---
+++ Assets/Scripts/AvaratHandle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+
+public class AvaratHandle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite[] framers;
+    private int currentFramer = 0;
+    private Image image;
+    float timer = 0.0f;
+    int seconds = 0;
+   
+    void Start()
+    {
+        image = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+        seconds = (int)(timer % 60);
+        //Debug.Log("timer:" + timer);
+        Debug.Log("seconds:" + seconds);
+
+        //image.sprite = framers[seconds%2];
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+            currentFramer++;
+            currentFramer = currentFramer % 2;
+            image.sprite = framers[currentFramer];
+            Debug.Log("currentFramer:" + currentFramer);
+            StartCoroutine("delay");
+
+
+
+        }
+    }
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(1);
+        currentFramer++;
+        currentFramer = currentFramer % 2;
+        image.sprite = framers[currentFramer];
+        Debug.Log("currentFramer:" + currentFramer);
+
+    }
+
+
+
+
+}
